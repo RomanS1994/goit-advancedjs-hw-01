547cda09e252c73c5de795bc7820b460716c33c9
Roman <140334590+RomanS1994@users.noreply.github.com>
2024-01-15 11:06:17 +0100

    Add new feature SimpleLightbox

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,32 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
-// Change code below this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-console.log(galleryItems);
+let list = document.querySelector('.gallery');
+
+/****** Створення і рендер розмітки на підставі масиву даних galleryItems і наданого шаблону елемента галереї. ******/
+
+const markup = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      ` <li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+          />
+        </a>
+      </li>`
+  )
+  .join('');
+
+list.insertAdjacentHTML('afterbegin', markup);
+var lightbox = new SimpleLightbox('.gallery a', {
+  // navText: ['←', '→'],
+  captionsData: 'alt',
+  captionDelay: 350,
+  animationSpeed: 600,
+});
