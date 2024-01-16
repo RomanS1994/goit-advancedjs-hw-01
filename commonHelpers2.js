659e8c006fff41f6434b69c19b586aacd91baf9c
RomanS1994 <140334590+RomanS1994@users.noreply.github.com>
2024-01-16 11:00:41 +0000

    Deploying to gh-pages from @ RomanS1994/goit-advancedjs-hw-01@9a329f631678b374019c907b156bc0c9f80b611b 🚀

--- commonHelpers2.js
+++ commonHelpers2.js
@@ -1,2 +1,2 @@
-import"./assets/modulepreload-polyfill-ec808ebb.js";/* empty css                      */import{P as o,l as a}from"./assets/vendor-ceec3c52.js";const m=document.querySelector("iframe"),e=new o(m),t="videoplayer-current-time";e.on("timeupdate",a(r=>{localStorage.setItem(t,JSON.stringify(r.seconds))},2e3));const s=localStorage.getItem(t);e.setCurrentTime(JSON.parse(s));
+import"./assets/modulepreload-polyfill-ec808ebb.js";/* empty css                      */import{P as o,l as a}from"./assets/vendor-ceec3c52.js";const m=document.querySelector("iframe"),e=new o(m),t="videoplayer-current-time";e.on("timeupdate",a(({seconds:r})=>{localStorage.setItem(t,r)},2e3));const l=localStorage.getItem(t)||0;e.setCurrentTime(JSON.parse(l));
 //# sourceMappingURL=commonHelpers2.js.map
